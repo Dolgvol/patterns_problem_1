882622ffd1d4a96fc56bc499fb1455212f2a6514
ivarWsl <ivarWsl@example.com>
2025-03-21 22:51:57 +0100

    solution 1 for problem 1

---
+++ problem_1.js
@@ -0,0 +1,46 @@
+
+const data = `city,population,area,density,country
+Shanghai,24256800,6340,3826,China
+Delhi,16787941,1484,11313,India
+Lagos,16060303,1171,13712,Nigeria
+Istanbul,14160467,5461,2593,Turkey
+Tokyo,13513734,2191,6168,Japan
+Sao Paulo,12038175,1521,7914,Brazil
+Mexico City,8874724,1486,5974,Mexico
+London,8673713,1572,5431,United Kingdom
+New York City,8537673,784,10892,United States
+Bangkok,8280925,1569,5279,Thailand`;
+
+if (data) {
+    // data preparing
+    const lines = data.split('\n');
+
+    const dataConfig = new Map([...lines[0].split(',').map((col, i) => [col, i]), ['relDensity', 5]]);
+    
+    const table = [...lines].slice(1, -1).map(line => line.split(','))
+        .toSorted((row1, row2) => row2[dataConfig.get('density')] - row1[dataConfig.get('density')]);
+
+    // table modification
+    const maxDensity = table[0][dataConfig.get('density')];
+    table.forEach(row => {
+        const relDensityValue = (Math.round((row[dataConfig.get('density')] * 100) / maxDensity)).toString();
+        row.push(relDensityValue);
+    });
+
+    // table rendering
+    const rowRenderConfig = [
+        { length: 18, method: 'padEnd' },
+        { length: 10, method: 'padStart' },
+        { length: 8, method: 'padStart' },
+        { length: 8, method: 'padStart' },
+        { length: 18, method: 'padStart' },
+        { length: 6, method: 'padStart' }
+    ];
+
+    table.forEach(row => {
+        const formattedRow = row
+            .map((cell, i) => cell[rowRenderConfig[i].method](rowRenderConfig[i].length))
+            .join('');
+        console.log(formattedRow);
+    });
+}
